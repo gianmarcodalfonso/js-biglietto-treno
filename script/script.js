1876565970836f0e39cf4032daf57cd46422a702
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-07-01 16:30:28 +0200

    [fix] minor fix to tripPrice.log

--- script/script.js
+++ script/script.js
@@ -4,7 +4,7 @@ let userKm = parseInt(prompt(`Inserisci la lunghezza del viaggio in km:`));
 let userAge = parseInt(prompt(`Inserisci la tua età:`));
 
 let tripPrice = userKm * kmPrice;
-console.log(`Costo del viaggio: ${tripPrice}€;`);
+console.log(`Costo del viaggio: ${tripPrice.toFixed(2)}€;`);
 
 if (userAge <= 18 || userAge >= 65) {
   console.log(`Hai diritto ad uno sconto;`);
