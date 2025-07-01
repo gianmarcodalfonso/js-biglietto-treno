cd42a3361f5621db2d90c28529af557d7e016d95
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-07-01 15:45:57 +0200

    [add] tripDiscounted condition

--- script/script.js
+++ script/script.js
@@ -4,4 +4,9 @@ let userKm = parseInt(prompt(`Inserisci la lunghezza del viaggio in km`));
 let userAge = parseInt(prompt(`Inserisci la tua età`));
 
 let tripPrice = userKm * kmPrice;
-console.log(tripPrice)
+console.log(`Costo del viaggio: ${tripPrice}€;`);
+
+if (userAge <= 18 || userAge >= 65) {
+  console.log(`Hai diritto ad uno sconto`);
+  let tripDiscounted;
+}
