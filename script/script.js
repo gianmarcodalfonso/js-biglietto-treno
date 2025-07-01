3683f8555542f304ed499d0caf2fedfe9aaee90a
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-07-01 15:33:11 +0200

    [add] tripPrice calc

--- script/script.js
+++ script/script.js
@@ -1,5 +1,7 @@
-const KmPrice = 0.21;
+const kmPrice = 0.21;
 
 let userKm = parseInt(prompt(`Inserisci la lunghezza del viaggio in km`));
 let userAge = parseInt(prompt(`Inserisci la tua età`));
 
+let tripPrice = userKm * kmPrice;
+console.log(tripPrice)
